b26780cdce880ac7f10e971fb5cce2c983012074
YoshiBNE <yoshiyasu_express@me.com>
2022-02-24 03:19:56 +1000

    change photos

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function changeBackground(sw) {
     let pht;
     let altname;
     if (sw == 0) {
-        pht = 'images/palm_leaves.png';
-        altname = 'palm_leaves.png';
+        pht = 'images/blackdog.jpg';
+        altname = 'my photo';
     } else {
-        pht = 'images/technology-background.jpg';
-        altname = 'technology-background.jpg';
+        pht = 'images/monkey.jpg';
+        altname = 'another photo of me';
     } 
     document.getElementById('topImage').src = pht;
     document.getElementById('topImage').alt = altname;
